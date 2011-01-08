fe309c7f13f6067dac0a7cfacfd8b15f93508342
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-01-08 19:17:47 +0100

    error handling in main

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@ int main(int ac, const char* av[]) {
   double* buffer;
   double relative_threshold, gated_loudness;
   int errcode = 0;
+  int result;
 
   CHECK_ERROR(ac != 2, "usage: r128-test FILENAME\n", 1, exit)
 
@@ -30,16 +31,17 @@ int main(int ac, const char* av[]) {
   CHECK_ERROR(!file, "Could not open input file!\n", 1, exit)
 
   st = ebur128_init( (size_t) file_info.frames, file_info.channels);
+  CHECK_ERROR(!st, "Could not initialize EBU R128!\n", 1, close_file)
 
-  buffer = (double*) malloc(48000
-                          * st->channels
-                          * sizeof(double));
+  buffer = (double*) malloc(48000 * st->channels * sizeof(double));
+  CHECK_ERROR(!buffer, "Could not allocate memory!\n", 1, destroy_ebur128)
   while ((nr_frames_read = sf_readf_double(file, buffer, 48000))) {
     nr_frames_read_all += nr_frames_read;
-    ebur128_write_frames(st, buffer, (size_t) nr_frames_read);
+    result = ebur128_write_frames(st, buffer, (size_t) nr_frames_read);
+    CHECK_ERROR(result, "Internal EBU R128 error!\n", 1, free_buffer)
   }
   CHECK_ERROR(file_info.frames != nr_frames_read_all,
-              "Could not read full file!\n", 1, close_file)
+              "Could not read full file!\n", 1, free_buffer)
 
   relative_threshold = ebur128_relative_threshold(st);
   gated_loudness = ebur128_gated_loudness(st, relative_threshold);
@@ -47,9 +49,11 @@ int main(int ac, const char* av[]) {
   fprintf(stderr, "relative threshold: %f LKFS\n", relative_threshold);
   fprintf(stderr, "gated loudness: %f LKFS\n", gated_loudness);
 
+free_buffer:
   free(buffer);
   buffer = NULL;
 
+destroy_ebur128:
   ebur128_destroy(&st);
 
 close_file:
