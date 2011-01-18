3c7a9bffe23c3316e4fd05e41dce17cd862827bb
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-01-18 15:00:03 +0100

    new mpg123 example

--- src/mpg123_example.c
+++ src/mpg123_example.c
@@ -3,10 +3,11 @@
 #include <float.h>
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <sndfile.h>
+#include <mpg123.h>
 
 #include "./ebur128.h"
 
@@ -19,8 +20,9 @@
 
 
 int main(int ac, char* const av[]) {
-  SF_INFO file_info;
-  SNDFILE* file;
+  mpg123_handle* mh = NULL;
+  long mh_rate;
+  int mh_channels, mh_encoding;
 
   int channels, samplerate;
   size_t nr_frames_read, nr_frames_read_all;
@@ -56,19 +58,34 @@ int main(int ac, char* const av[]) {
     }
   }
 
+  result = mpg123_init();
+  if (result != MPG123_OK) {
+    fprintf(stderr, "Could not initialize mpg123!");
+    return 1;
+  }
+
   segment_loudness = calloc((size_t) (ac - optind), sizeof(double));
   segment_peaks = calloc((size_t) (ac - optind), sizeof(double));
   for (i = optind; i < ac; ++i) {
     segment_loudness[i - optind] = DBL_MAX;
-    memset(&file_info, '\0', sizeof(file_info));
-    if (av[i][0] == '-' && av[1][1] == '\0') {
-      file = sf_open_fd(0, SFM_READ, &file_info, SF_FALSE);
-    } else {
-      file = sf_open(av[i], SFM_READ, &file_info);
-    }
-    CHECK_ERROR(!file, "Could not open file!\n", 1, endloop)
-    channels = file_info.channels;
-    samplerate = file_info.samplerate;
+    mh = mpg123_new(NULL, &result);
+    CHECK_ERROR(!mh, "Could not create mpg123 handler!\n", 1, close_file)
+    result = mpg123_open(mh, av[i]);
+    CHECK_ERROR(result != MPG123_OK, "Could not open input file!\n", 1, close_file)
+    result = mpg123_getformat(mh, &mh_rate, &mh_channels, &mh_encoding);
+    CHECK_ERROR(result != MPG123_OK, "mpg123_getformat failed!\n", 1, close_file)
+    result = mpg123_format_none(mh);
+    CHECK_ERROR(result != MPG123_OK, "mpg123_format_none failed!\n", 1, close_file)
+    result = mpg123_format(mh, mh_rate, mh_channels, MPG123_ENC_FLOAT_32);
+    CHECK_ERROR(result != MPG123_OK, "mpg123_format failed!\n", 1, close_file)
+    result = mpg123_close(mh);
+    result = mpg123_open(mh, av[i]);
+    CHECK_ERROR(result != MPG123_OK, "Could not open input file!\n", 1, close_file)
+    result = mpg123_getformat(mh, &mh_rate, &mh_channels, &mh_encoding);
+    CHECK_ERROR(result != MPG123_OK, "mpg123_getformat failed!\n", 1, close_file)
+
+    channels = mh_channels;
+    samplerate = (int) mh_rate;
     nr_frames_read_all = 0;
 
     if (!st) {
@@ -77,43 +94,6 @@ int main(int ac, char* const av[]) {
                         EBUR128_MODE_I |
                         (calculate_lra ? EBUR128_MODE_LRA : 0));
       CHECK_ERROR(!st, "Could not initialize EBU R128!\n", 1, close_file)
-
-      result = sf_command(file, SFC_GET_CHANNEL_MAP_INFO,
-                                (void*) st->channel_map,
-                                channels * (int) sizeof(int));
-      /* If sndfile found a channel map, set it directly to st->channel_map */
-      if (result == SF_TRUE) {
-        size_t j;
-        for (j = 0; j < st->channels; ++j) {
-          switch (st->channel_map[j]) {
-            case SF_CHANNEL_MAP_INVALID:
-              st->channel_map[j] = EBUR128_UNUSED;         break;
-            case SF_CHANNEL_MAP_MONO:
-              st->channel_map[j] = EBUR128_CENTER;         break;
-            case SF_CHANNEL_MAP_LEFT:
-              st->channel_map[j] = EBUR128_LEFT;           break;
-            case SF_CHANNEL_MAP_RIGHT:
-              st->channel_map[j] = EBUR128_RIGHT;          break;
-            case SF_CHANNEL_MAP_CENTER:
-              st->channel_map[j] = EBUR128_CENTER;         break;
-            case SF_CHANNEL_MAP_REAR_LEFT:
-              st->channel_map[j] = EBUR128_LEFT_SURROUND;  break;
-            case SF_CHANNEL_MAP_REAR_RIGHT:
-              st->channel_map[j] = EBUR128_RIGHT_SURROUND; break;
-            default:
-              st->channel_map[j] = EBUR128_UNUSED; break;
-          }
-        }
-      /* Special case seq-3341-6-5channels-16bit.wav.
-       * Set channel map with function ebur128_set_channel_map. */
-      } else if (channels == 5) {
-        int channel_map_five[] = {EBUR128_LEFT,
-                                  EBUR128_RIGHT,
-                                  EBUR128_CENTER,
-                                  EBUR128_LEFT_SURROUND,
-                                  EBUR128_RIGHT_SURROUND};
-        ebur128_set_channel_map(st, channel_map_five);
-      }
     } else {
       CHECK_ERROR(st->channels != (size_t) channels ||
                   st->samplerate != (size_t) samplerate,
@@ -126,8 +106,13 @@ int main(int ac, char* const av[]) {
     CHECK_ERROR(!buffer, "Could not allocate memory!\n", 1, close_file)
     segment_peaks[i - optind] = 0.0;
     for (;;) {
-      nr_frames_read = (size_t) sf_readf_float(file, buffer,
-                                             (sf_count_t) st->samplerate);
+      result = mpg123_read(mh, (unsigned char*) buffer,
+                               st->samplerate * st->channels * sizeof(float),
+                               &nr_frames_read);
+      CHECK_ERROR(result != MPG123_OK &&
+                  result != MPG123_DONE,
+                  "Internal MPG123 error!\n", 1, free_buffer)
+      nr_frames_read /= st->channels * sizeof(float);
       if (!nr_frames_read) break;
       if (rgtag_exe) {
         size_t j;
@@ -141,10 +126,6 @@ int main(int ac, char* const av[]) {
       nr_frames_read_all += nr_frames_read;
       result = ebur128_add_frames_float(st, buffer, (size_t) nr_frames_read);
       CHECK_ERROR(result, "Internal EBU R128 error!\n", 1, free_buffer)
-    }
-    if (file && (size_t) file_info.frames != nr_frames_read_all) {
-      fprintf(stderr, "Warning: Could not read full file"
-                              " or determine right length!\n");
     }
 
     segment_loudness[i - optind] = ebur128_gated_loudness_segment(st);
@@ -163,12 +144,9 @@ int main(int ac, char* const av[]) {
     buffer = NULL;
 
   close_file:
-    if (sf_close(file)) {
-      fprintf(stderr, "Could not close input file!\n");
-    }
-    file = NULL;
-
-  endloop: ;
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mh = NULL;
   }
 
   if (st && calculate_lra) {
@@ -204,6 +182,8 @@ int main(int ac, char* const av[]) {
     free(segment_loudness);
   if (segment_peaks)
     free(segment_peaks);
+
+  mpg123_exit();
 
 exit:
   return errcode;
