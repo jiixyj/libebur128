c6acac3642787a595b48a2eb7011098eac40a41c
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-01-18 18:30:32 +0100

    add support for Win64

---
+++ cmake/x86_64-w64-mingw32.cmake
@@ -0,0 +1,8 @@
+# Based on http://www.cmake.org/Wiki/CmakeMingw
+
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
