1a9249c1086a487be58ad3733ec43e474e71f505
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-01-23 00:06:03 +0100

    more options and better error handling for script

--- src/r128-tag.py
+++ src/r128-tag.py
@@ -10,19 +10,53 @@ except ImportError:
   import Queue as queue
 import signal
 import string
+import getopt
+import time
 
 import rgtag
 
-if len(sys.argv) == 1:
-  print("usage: r128-tag <directory>\n"
-        "  r128-tag will scan a directory recursively and tag all\n"
-        "  music files with ReplayGain compatible tags.")
+def signal_handler(signal, frame):
+  print('\nExiting!')
+  sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+def usage():
+  print("usage: r128-tag [-r] <directory(ies)> ...\n"
+        "       r128-tag <file(s)> ...\n\n"
+        "    -h: show this help\n"
+        "    -r: scan directory(ies) recursively\n\n"
+        "  r128-tag scans music files and tags them with ReplayGain "
+                                                           "compatible tags.\n"
+        "  If more than one directory is given, all available cores will be"
+         " used.")
+
+recursive = False
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "hr")
+except getopt.GetoptError:
+  usage()
+  sys.exit(1)
+for opt, arg in opts:
+  if opt in ("-h"):
+    usage()
+    sys.exit(1)
+  elif opt in ("-r"):
+    recursive = True
+
+if len(args) == 0:
+  usage()
   sys.exit(1)
 
 def worker():
   while True:
     item = q.get()
     item.wait()
+    if item.returncode != 0:
+      q.task_done()
+      w.get()
+      w.task_done()
+      return
     stdoutdata, stderrdata = item.communicate()
     rginfo = stdoutdata.decode('ascii').splitlines()
     q.task_done()
@@ -37,27 +71,12 @@ def worker():
                                                   float(rgdata[2]),
                                                   float(rgdata[3]))
     except:
-      pass
+      print("Tagging error!")
+      w.task_done()
+      return
     w.task_done()
 
-topdir = os.path.abspath(os.path.dirname(sys.argv[0]))
-
-number_threads = 1
-try:
-  import multiprocessing
-  number_threads = multiprocessing.cpu_count()
-except (ImportError,NotImplementedError):
-  pass
-
-w = queue.Queue(number_threads)
-q = queue.Queue(number_threads)
-
-t = threading.Thread(target=worker)
-t.daemon = True
-t.start()
-
-for root, dirs, files in os.walk(sys.argv[1]):
-  currentdir = os.getcwd()
+def process_dir(root, files):
   files_mp3 = [elem for elem in files if elem[-4:].find(".mp3") == 0]
   files_snd = [elem for elem in files if elem[-4:].find(".ogg") == 0 or
                                          elem[-4:].find(".oga") == 0 or
@@ -78,15 +97,73 @@ for root, dirs, files in os.walk(sys.argv[1]):
     r128_scanner = 'r128-musepack'
     r128_files += files_mpc
   else:
-    continue
+    return False
 
   if len(r128_files) == 0:
-    continue
+    return False
   w.put([root] + r128_files)
-  pipe = subprocess.Popen([os.path.join(topdir, r128_scanner), '-t'] + r128_files,
-                          stdout=subprocess.PIPE,
-                          cwd=root)
+  try:
+    pipe = subprocess.Popen([os.path.join(topdir, r128_scanner), '-t'] +
+                                                                     r128_files,
+                            stdout=subprocess.PIPE,
+                            cwd=root)
+  except OSError:
+    print("Error starting scanner " + r128_scanner + "!")
+    sys.exit(1)
+
   q.put(pipe)
+  return True
+
+
+
+
+topdir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+number_threads = 1
+try:
+  import multiprocessing
+  number_threads = multiprocessing.cpu_count()
+except (ImportError,NotImplementedError):
+  pass
+
+# Process either directories _or_ files
+all_files = True
+all_directory = True
+for filename in args:
+  if os.path.isdir(filename):
+    all_files = False
+  elif os.path.isfile(filename):
+    all_directory = False
+  else:
+    print("Error opening file!")
+    sys.exit(1)
+
+if not all_files and not all_directory:
+  usage()
+  sys.exit(1)
+if all_files and recursive:
+  usage()
+  sys.exit(1)
+
+
+w = queue.Queue(number_threads)
+q = queue.Queue(number_threads)
+
+t = threading.Thread(target=worker)
+t.daemon = True
+t.start()
+
+if all_directory:
+  for directory in args:
+    if recursive:
+      for root, dirs, files in os.walk(args[0]):
+        process_dir(root, files)
+    else:
+      if not process_dir(args[0], os.listdir(args[0])):
+        print("No files to scan!")
+else:
+  process_dir(os.getcwd(), args)
+
 
 w.join()
 q.join()
