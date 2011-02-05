7bea53fc3d7d5070c7ee640fec8839eac73e0dc3
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-02-05 15:29:03 +0100

    add -s and -m options for the scanner

--- src/scanner.c
+++ src/scanner.c
@@ -39,7 +39,7 @@ void calculate_gain_of_file(void* user, void* user_data) {
   int calculate_lra = gd->calculate_lra, tag_rg = gd->tag_rg;
 
   struct input_handle* ih = input_handle_init();
-  size_t nr_frames_read_all = 0;
+  size_t nr_frames_read, nr_frames_read_all = 0;
 
   ebur128_state* st = NULL;
   float* buffer = NULL;
@@ -72,9 +72,7 @@ void calculate_gain_of_file(void* user, void* user_data) {
   CHECK_ERROR(result, "Could not allocate memory!\n", 1, close_file)
   segment_peaks[i] = 0.0;
   buffer = input_get_buffer(ih);
-  for (;;) {
-    size_t nr_frames_read = input_read_frames(ih);
-    if (!nr_frames_read) break;
+  while ((nr_frames_read = input_read_frames(ih))) {
     if (tag_rg) {
       size_t j;
       for (j = 0; j < (size_t) nr_frames_read * st->channels; ++j) {
@@ -177,7 +175,88 @@ exit:
   return errcode;
 }
 
-int interval_loudness(struct gain_data* gd, int no_files) { }
+int interval_loudness(struct gain_data* gd, int no_files, int mode) {
+  int errcode = 0, i, result;
+  ebur128_state* st = NULL;
+  float* buffer = NULL;
+  size_t nr_frames_read;
+  size_t frames_counter, frames_needed;
+
+  CHECK_ERROR(input_init_library(),
+              "Could not initialize input library!", 1, exit)
+
+
+  for (i = 0; i < no_files; ++i) {
+    struct input_handle* ih = input_handle_init();
+
+    result = input_open_file(ih, gd->file_names[i]);
+    CHECK_ERROR(result, "Could not open file!\n", 1, endloop)
+
+    if (!st) {
+      st = ebur128_init(input_get_channels(ih),
+                        input_get_samplerate(ih),
+                        (size_t) mode);
+      CHECK_ERROR(!st, "Could not initialize EBU R128!\n", 1, close_file)
+      frames_counter = 0;
+    } else {
+      if (!ebur128_change_parameters(st, input_get_channels(ih),
+                                         input_get_samplerate(ih))) {
+        frames_counter = 0;
+      }
+    }
+
+    /* Special case seq-3341-6-5channels-16bit.wav.
+     * Set channel map with function ebur128_set_channel. */
+    result = input_set_channel_map(ih, st);
+    if (result && st->channels == 5) {
+      ebur128_set_channel(st, 0, EBUR128_LEFT);
+      ebur128_set_channel(st, 1, EBUR128_RIGHT);
+      ebur128_set_channel(st, 2, EBUR128_CENTER);
+      ebur128_set_channel(st, 3, EBUR128_LEFT_SURROUND);
+      ebur128_set_channel(st, 4, EBUR128_RIGHT_SURROUND);
+    }
+
+    frames_needed = ((mode == EBUR128_MODE_M) ? gd->momentary_interval
+                                              : gd->shortterm_interval) *
+                    st->samplerate;
+
+    result = input_allocate_buffer(ih);
+    CHECK_ERROR(result, "Could not allocate memory!\n", 1, close_file)
+    buffer = input_get_buffer(ih);
+    while ((nr_frames_read = input_read_frames(ih))) {
+      float* tmp_buffer = buffer;
+      while (nr_frames_read > 0) {
+        if (frames_counter + nr_frames_read >= frames_needed) {
+          result = ebur128_add_frames_float(st, tmp_buffer,
+                                            frames_needed - frames_counter);
+          CHECK_ERROR(result, "Internal EBU R128 error!\n", 1, free_buffer)
+          tmp_buffer += (frames_needed - frames_counter) * st->channels;
+          nr_frames_read -= frames_needed - frames_counter;
+          frames_counter = 0;
+          printf("%f\n", (mode == EBUR128_MODE_M) ?
+                                        ebur128_loudness_momentary(st)
+                                      : ebur128_loudness_shortterm(st));
+        } else {
+          result = ebur128_add_frames_float(st, tmp_buffer, nr_frames_read);
+          CHECK_ERROR(result, "Internal EBU R128 error!\n", 1, free_buffer)
+          tmp_buffer += (nr_frames_read) * st->channels;
+          frames_counter += nr_frames_read;
+          nr_frames_read = 0;
+        }
+      }
+    }
+  free_buffer:
+    input_free_buffer(ih);
+  close_file:
+    input_close_file(ih);
+  endloop:
+    input_handle_destroy(&ih);
+  }
+  input_exit_library();
+  ebur128_destroy(&st);
+exit:
+  return errcode;
+}
 
 int main(int ac, char* const av[]) {
   int errcode = 0, c;
@@ -232,25 +311,24 @@ int main(int ac, char* const av[]) {
     fprintf(stderr, "-m and -s can not be specified together!\n");
     return 1;
   }
-  if (gd.momentary_interval > 0.0 || gd.shortterm_interval > 0.0) {
-    if (ac - optind != 1) {
-      fprintf(stderr, "-m or -s mode can only take one input file!\n");
-      return 1;
-    }
-  }
 
   gd.file_names = &av[optind];
-  gd.segment_loudness = calloc((size_t) (ac - optind), sizeof(double));
-  gd.segment_peaks = calloc((size_t) (ac - optind), sizeof(double));
-  gd.library_states = calloc((size_t) (ac - optind), sizeof(ebur128_state*));
+  if (gd.momentary_interval > 0.0 || gd.shortterm_interval > 0.0) {
+    int mode = gd.momentary_interval > 0.0 ? EBUR128_MODE_M : EBUR128_MODE_S;
+    interval_loudness(&gd, ac - optind, mode);
+  } else {
+    gd.segment_loudness = calloc((size_t) (ac - optind), sizeof(double));
+    gd.segment_peaks = calloc((size_t) (ac - optind), sizeof(double));
+    gd.library_states = calloc((size_t) (ac - optind), sizeof(ebur128_state*));
 
-  if (loudness_or_lra(&gd, ac - optind)) {
-    errcode = 1;
+    if (loudness_or_lra(&gd, ac - optind)) {
+      errcode = 1;
+    }
+
+    free(gd.library_states);
+    free(gd.segment_loudness);
+    free(gd.segment_peaks);
   }
-
-  free(gd.library_states);
-  free(gd.segment_loudness);
-  free(gd.segment_peaks);
 
 exit:
   return errcode;
