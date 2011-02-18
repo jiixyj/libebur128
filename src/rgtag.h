ca2673d0b9734d0d02e3b05e7e67689b7de69ee8
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-02-18 04:40:51 +0100

    add rgtag.h

---
+++ src/rgtag.h
@@ -0,0 +1,20 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _RGTAG_H_
+#define _RGTAG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void set_rg_info(const char* filename,
+                 double track_gain,
+                 double track_peak,
+                 int album_mode,
+                 double album_gain,
+                 double album_peak);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _RGTAG_H_ */
