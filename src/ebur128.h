a9bf3c800585b067e65b12e9d27eb739b25321bf
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-02-26 02:03:26 +0100

    add doxygen comments to ebur128.h

--- src/ebur128.h
+++ src/ebur128.h
@@ -2,23 +2,34 @@
 #ifndef _EBUR128_H_
 #define _EBUR128_H_
 
+/** \file ebur128.h
+ *  \brief libebur128 - a library for loudness measurement according to
+ *         the EBU R128 standard.
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <string.h>
 
-/* This can be replaced by any BSD-like queue implementation */
+/* This can be replaced by any BSD-like queue implementation. */
 #include "queue.h"
 
+/** @cond SLIST
+ *  Declare a linked list of double for out block energies.
+ */
 SLIST_HEAD(ebur128_double_queue, ebur128_dq_entry);
 struct ebur128_dq_entry {
   double z;
   SLIST_ENTRY(ebur128_dq_entry) entries;
 };
+/** @endcond */
 
-/* Use these values when setting the channel map */
-enum channels {
+/** \enum channel
+ *  Use these values when setting the channel map with ebur128_set_channel().
+ */
+enum channel {
   EBUR128_UNUSED = 0,
   EBUR128_LEFT,
   EBUR128_RIGHT,
@@ -27,61 +38,134 @@ enum channels {
   EBUR128_RIGHT_SURROUND
 };
 
-/* Use these values in ebur128_init (xor'ed) */
+/** \enum mode
+ *  Use these values in ebur128_init (xor'ed). Try to use the lowest possible
+ *  mode that suits your needs, as performance will be better.
+ */
 enum mode {
-  EBUR128_MODE_M         =  1, /* can call ebur128_loudness_momentary */
-  EBUR128_MODE_S         =  3, /* can call ebur128_loudness_shortterm */
-  EBUR128_MODE_I         =  5, /* can call ebur128_gated_loudness_*   */
-  EBUR128_MODE_LRA       = 11  /* can call ebur128_loudness_range     */
+  EBUR128_MODE_M         =  1, /**< can call ebur128_loudness_momentary */
+  EBUR128_MODE_S         =  3, /**< can call ebur128_loudness_shortterm */
+  EBUR128_MODE_I         =  5, /**< can call ebur128_gated_loudness_*   */
+  EBUR128_MODE_LRA       = 11  /**< can call ebur128_loudness_range     */
 };
 
+/** \brief Contains information about the state of a loudness measurement.
+ *
+ *  You should not need to modify this struct directly. The contents can change
+ *  between library versions. The following fields will always be available for
+ *  reading:
+ *    - mode
+ *    - channels
+ *    - samplerate
+ */
 typedef struct {
+  /** The current mode. */
   int mode;
+  /** Filtered audio data (used as ring buffer). */
   double* audio_data;
+  /** Size of audio_data array. */
   size_t audio_data_frames;
+  /** Current index for audio_data. */
   size_t audio_data_index;
+  /** How many frames are needed for a gating block. Will correspond to 400ms
+   *  of audio at initialization, and 200ms after the first block (50% overlap
+   *  as specified in R128). */
   size_t needed_frames;
+  /** The number of channels. */
   size_t channels;
+  /** The channel map. Has as many elements as there are channels. */
   int* channel_map;
+  /** The sample rate. */
   size_t samplerate;
+  /** How many samples fit in 200ms (rounded). */
   size_t samples_in_200ms;
+  /** BS.1770 filter coefficients (denominator). */
   double* a;
+  /** BS.1770 filter coefficients (nominator). */
   double* b;
+  /** BS.1770 filter state. */
   double** v;
+  /** Linked list of block energies. */
   struct ebur128_double_queue block_list;
+  /** Linked list of 3s-block energies, used to calculate LRA. */
   struct ebur128_double_queue short_term_block_list;
+  /** Keeps track of when a new short term block is needed. */
   size_t short_term_frame_counter;
 } ebur128_state;
 
+/** \brief Initialize library state.
+ *
+ *  @param channels the number of channels.
+ *  @param samplerate the sample rate.
+ *  @param mode see the mode enum for possible values.
+ *  @return an initialized library state.
+ */
 ebur128_state* ebur128_init(size_t channels, size_t samplerate, int mode);
-int ebur128_destroy(ebur128_state** st);
 
-/* Set channel "channel_number" (zero based index) to value, where value is
- * from the "channels" enum. Returns 1 if channel_number is invalid.
- * The default is: 0 - EBUR128_LEFT
- *                 1 - EBUR128_RIGHT
- *                 2 - EBUR128_CENTER
- *                 3 - EBUR128_UNUSED
- *                 4 - EBUR128_LEFT_SURROUND
- *                 5 - EBUR128_RIGHT_SURROUND  */
+/** \brief Destroy library state.
+ *
+ *  @param st library state.
+ *  @param mode see the mode enum for possible values.
+ */
+void ebur128_destroy(ebur128_state** st);
+
+/** \brief Set channel type.
+ *
+ *  The default is:
+ *  - 0 -> EBUR128_LEFT
+ *  - 1 -> EBUR128_RIGHT
+ *  - 2 -> EBUR128_CENTER
+ *  - 3 -> EBUR128_UNUSED
+ *  - 4 -> EBUR128_LEFT_SURROUND
+ *  - 5 -> EBUR128_RIGHT_SURROUND
+ *
+ *  @param st library state.
+ *  @param channel_number zero based channel index.
+ *  @param value channel type from the "channel" enum.
+ *  @return
+ *    - 0 on success.
+ *    - 1 if invalid channel index.
+ */
 int ebur128_set_channel(ebur128_state* st, size_t channel_number, int value);
-/* Note that the channel map will be reset when setting a different number of
- * channels. The current unfinished block will be lost.
- * Returns 1 on memory allocation error. The state will be invalid then.
- * Returns 2 if channels and samplerate were not changed. */
+
+/** \brief Change library parameters.
+ *
+ *  Note that the channel map will be reset when setting a different number of
+ *  channels. The current unfinished block will be lost.
+ *
+ *  @param st library state.
+ *  @param channels new number of channels.
+ *  @param samplerate new sample rate.
+ *  @return
+ *    - 0 on success.
+ *    - 1 on memory allocation error. The state will be invalid then.
+ *    - 2 if channels and sample rate were not changed.
+ */
 int ebur128_change_parameters(ebur128_state* st,
                               size_t channels,
                               size_t samplerate);
 
+/** \brief Add frames to be processed.
+ *
+ *  @param st library state.
+ *  @param src array of source frames. Channels must be interleaved.
+ *  @param frames number of frames. Not number of samples!
+ *  @return
+ *    - 0 on success.
+ *    - 1 on memory allocation error.
+ */
 int ebur128_add_frames_short(ebur128_state* st,
                              const short* src,
                              size_t frames);
+/** \brief See \ref ebur128_add_frames_short */
 int ebur128_add_frames_int(ebur128_state* st,
                              const int* src,
                              size_t frames);
+/** \brief See \ref ebur128_add_frames_short */
 int ebur128_add_frames_float(ebur128_state* st,
                              const float* src,
                              size_t frames);
+/** \brief See \ref ebur128_add_frames_short */
 int ebur128_add_frames_double(ebur128_state* st,
                              const double* src,
                              size_t frames);
