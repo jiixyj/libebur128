13b5ec549a7a51c396195557870befb8147a700f
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-07-02 16:58:28 +0200

    improve error handling (just header, does not build)

--- libebur128/ebur128.h
+++ libebur128/ebur128.h
@@ -31,6 +31,18 @@ enum channel {
   EBUR128_DUAL_MONO       /**< a channel that is counted twice */
 };
 
+/** \enum error
+ *  Error return values.
+ */
+enum error {
+  EBUR128_SUCCESS = 0,
+  EBUR128_ERROR_NEGINF,
+  EBUR128_ERROR_NOMEM,
+  EBUR128_ERROR_INVALID_MODE,
+  EBUR128_ERROR_INVALID_CHANNEL_INDEX,
+  EBUR128_ERROR_NO_CHANGE
+};
+
 /** \enum mode
  *  Use these values in ebur128_init (or'ed). Try to use the lowest possible
  *  modes that suit your needs, as performance will be better.
@@ -96,8 +108,8 @@ void ebur128_destroy(ebur128_state** st);
  *  @param channel_number zero based channel index.
  *  @param value channel type from the "channel" enum.
  *  @return
- *    - 0 on success.
- *    - 1 if invalid channel index.
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_INVALID_CHANNEL_INDEX if invalid channel index.
  */
 int ebur128_set_channel(ebur128_state* st, size_t channel_number, int value);
 
@@ -110,10 +122,10 @@ int ebur128_set_channel(ebur128_state* st, size_t channel_number, int value);
  *  @param channels new number of channels.
  *  @param samplerate new sample rate.
  *  @return
- *    - 0 on success.
- *    - 1 on memory allocation error. The state will be invalid and must be
- *        destroyed.
- *    - 2 if channels and sample rate were not changed.
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NOMEM on memory allocation error. The state will be
+ *      invalid and must be destroyed.
+ *    - EBUR128_ERROR_NO_CHANGE if channels and sample rate were not changed.
  */
 int ebur128_change_parameters(ebur128_state* st,
                               size_t channels,
@@ -125,8 +137,8 @@ int ebur128_change_parameters(ebur128_state* st,
  *  @param src array of source frames. Channels must be interleaved.
  *  @param frames number of frames. Not number of samples!
  *  @return
- *    - 0 on success.
- *    - 1 on memory allocation error.
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NOMEM on memory allocation error.
  */
 int ebur128_add_frames_short(ebur128_state* st,
                              const short* src,
@@ -147,60 +159,97 @@ int ebur128_add_frames_double(ebur128_state* st,
 /** \brief Get global integrated loudness in LUFS.
  *
  *  @param st library state.
- *  @return integrated loudness in LUFS. NaN if mode "EBUR128_MODE_I" has not
- *          been set.
+ *  @param out integrated loudness in LUFS. Will not be changed if result is
+ *             negative infinity. EBUR128_ERROR_NEGINF will be returned in this
+ *             case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NEGINF if result is negative infinity.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_I" has not been set.
  */
-double ebur128_loudness_global(ebur128_state* st);
+int ebur128_loudness_global(ebur128_state* st, double* out);
 /** \brief Get global integrated loudness in LUFS across multiple instances.
  *
  *  @param sts array of library states.
  *  @param size length of sts
- *  @return integrated loudness in LUFS. NaN if mode "EBUR128_MODE_I" has not
- *          been set.
+ *  @param out integrated loudness in LUFS. Will not be changed if result is
+ *             negative infinity. EBUR128_ERROR_NEGINF will be returned in this
+ *             case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NEGINF if result is negative infinity.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_I" has not been set.
  */
-double ebur128_loudness_global_multiple(ebur128_state** sts, size_t size);
+int ebur128_loudness_global_multiple(ebur128_state** sts, size_t size,
+                                     double* out);
 
 /** \brief Get momentary loudness (last 400ms) in LUFS.
  *
  *  @param st library state.
- *  @return momentary loudness in LUFS.
+ *  @param out momentary loudness in LUFS. Will not be changed if result is
+ *             negative infinity. EBUR128_ERROR_NEGINF will be returned in this
+ *             case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NEGINF if result is negative infinity.
  */
-double ebur128_loudness_momentary(ebur128_state* st);
+int ebur128_loudness_momentary(ebur128_state* st, double* out);
 /** \brief Get short-term loudness (last 3s) in LUFS.
  *
  *  @param st library state.
- *  @return short-term loudness in LUFS. NaN if mode "EBUR128_MODE_S" has not
- *          been set.
+ *  @param out short-term loudness in LUFS. Will not be changed if result is
+ *             negative infinity. EBUR128_ERROR_NEGINF will be returned in this
+ *             case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NEGINF if result is negative infinity.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_S" has not been set.
  */
-double ebur128_loudness_shortterm(ebur128_state* st);
+int ebur128_loudness_shortterm(ebur128_state* st, double* out);
 
 /** \brief Get loudness range (LRA) of programme in LU.
  *
  *  Calculates loudness range according to EBU 3342.
  *
  *  @param st library state.
- *  @return loudness range (LRA) in LU. NaN if memory allocation fails or mode
- *          "EBUR128_MODE_LRA" has not been set.
+ *  @param out loudness range (LRA) in LU. Will not be changed in case of
+ *             error. EBUR128_ERROR_NOMEM or EBUR128_ERROR_INVALID_MODE will be
+ *             returned in this case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NOMEM in case of memory allocation error.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_LRA" has not been set.
  */
-double ebur128_loudness_range(ebur128_state* st);
+int ebur128_loudness_range(ebur128_state* st, double* out);
 /** \brief Get loudness range (LRA) in LU across multiple instances.
  *
  *  Calculates loudness range according to EBU 3342.
  *
  *  @param sts array of library states.
  *  @param size length of sts
- *  @return loudness range (LRA) in LU. NaN if memory allocation fails or mode
- *          "EBUR128_MODE_LRA" has not been set.
+ *  @param out loudness range (LRA) in LU. Will not be changed in case of
+ *             error. EBUR128_ERROR_NOMEM or EBUR128_ERROR_INVALID_MODE will be
+ *             returned in this case.
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_NOMEM in case of memory allocation error.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_LRA" has not been set.
  */
-double ebur128_loudness_range_multiple(ebur128_state** sts, size_t size);
+int ebur128_loudness_range_multiple(ebur128_state** sts, size_t size,
+                                    double* out);
 
 /** \brief Get maximum sample peak of selected channel in float format.
  *
  *  @param st library state
  *  @param channel_number channel to analyse
- *  @return maximum sample peak in float format (1.0 is 0 dBFS)
+ *  @param out maximum sample peak in float format (1.0 is 0 dBFS)
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_SAMPLE_PEAK" has not
+ *      been set.
+ *    - EBUR128_ERROR_INVALID_CHANNEL_INDEX if invalid channel index.
  */
-double ebur128_sample_peak(ebur128_state* st, size_t channel_number);
+int ebur128_sample_peak(ebur128_state* st, size_t channel_number, double* out);
 #if EBUR128_USE_SPEEX_RESAMPLER
 /** \brief Get maximum true peak of selected channel in float format.
  *
@@ -210,9 +259,14 @@ double ebur128_sample_peak(ebur128_state* st, size_t channel_number);
  *
  *  @param st library state
  *  @param channel_number channel to analyse
- *  @return maximum true peak in float format (1.0 is 0 dBFS)
+ *  @param out maximum true peak in float format (1.0 is 0 dBFS)
+ *  @return
+ *    - EBUR128_SUCCESS on success.
+ *    - EBUR128_ERROR_INVALID_MODE if mode "EBUR128_MODE_TRUE_PEAK" has not
+ *      been set.
+ *    - EBUR128_ERROR_INVALID_CHANNEL_INDEX if invalid channel index.
  */
-double ebur128_true_peak(ebur128_state* st, size_t channel_number);
+int ebur128_true_peak(ebur128_state* st, size_t channel_number, double* out);
 #endif
 
 #ifdef __cplusplus
