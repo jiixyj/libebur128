65073b69d68c935eecbf1267965de589e95e5dcb
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-10-17 10:50:15 +0200

    fix toolchain files for fedora cross compiler

--- cmake/i686-w64-mingw32.cmake
+++ cmake/i686-w64-mingw32.cmake
@@ -6,9 +6,10 @@ SET(CMAKE_SYSTEM_NAME Windows)
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32)
+SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32/sys-root/mingw)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
--- cmake/x86_64-w64-mingw32.cmake
+++ cmake/x86_64-w64-mingw32.cmake
@@ -6,6 +6,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
