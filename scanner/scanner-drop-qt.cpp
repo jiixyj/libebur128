8c0b4f4b9863047b2f76547934d0b1a77810405c
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-10-19 18:32:07 +0200

    Qt version can scan

--- scanner/scanner-drop-qt.cpp
+++ scanner/scanner-drop-qt.cpp
@@ -3,13 +3,23 @@
 #include <iostream>
 #include <glib.h>
 
+extern "C" {
+#include "input.h"
+#include "filetree.h"
+#include "scanner-tag.h"
+#include "scanner-common.h"
+}
+
 #include "logo.h"
 
 gboolean verbose = TRUE;
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent, Qt::FramelessWindowHint | Qt::WindowSystemMenuHint),
-      dragPosition()    
+      dragPosition(),
+      worker_thread_(NULL),
+      gui_update_thread_(this),
+      logo_rotation_timer(NULL)
 {
     setMinimumSize(130, 130);
     setMaximumSize(0, 0);
@@ -28,18 +38,37 @@ MainWindow::MainWindow(QWidget *parent)
     render_area_ = new RenderArea;
     progress_bar_ = new QProgressBar;
     progress_bar_->setFixedHeight(15);
+    progress_bar_->setMaximum(130);
     progress_bar_->setTextVisible(false);
 
     layout->addWidget(render_area_);
     layout->addWidget(progress_bar_);
     setLayout(layout);
+
+    setAcceptDrops(true);
+
+    connect(&gui_update_thread_, SIGNAL(setProgressBar(int)),
+            this, SLOT(setProgressBar(int)));
+    connect(&gui_update_thread_, SIGNAL(rotateLogo()),
+            this, SLOT(rotateLogo()));
+    connect(&gui_update_thread_, SIGNAL(resetLogo()),
+            this, SLOT(resetLogo()));
+    connect(this, SIGNAL(stopGUIThread()),
+            &gui_update_thread_, SLOT(stopThread()));
+    gui_update_thread_.start();
+}
+
+MainWindow::~MainWindow()
+{
+    emit stopGUIThread();
+    gui_update_thread_.wait();
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton) {
         dragPosition = event->globalPos() - frameGeometry().topLeft();
-        // setCursor(Qt::CursorShape(20));
+        setCursor(Qt::ClosedHandCursor);
         event->accept();
     }
 }
@@ -60,14 +89,55 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *event)
     }
 }
 
-void MainWindow::resizeEvent(QResizeEvent *event)
+void MainWindow::dragEnterEvent(QDragEnterEvent *event)
 {
-    event->accept();
+    if (event->mimeData()->hasFormat("text/uri-list"))
+        event->acceptProposedAction();
+}
+
+void MainWindow::dropEvent(QDropEvent *event)
+{
+    QList<QUrl> urls = event->mimeData()->urls();
+    if (!worker_thread_) {
+        worker_thread_ = new WorkerThread(urls);
+        worker_thread_->start();
+        connect(worker_thread_, SIGNAL(finished()),
+                this, SLOT(cleanUpThread()));
+    }
+    event->acceptProposedAction();
 }
 
 QSize MainWindow::sizeHint() const
 {
     return QSize(130, 130);
+}
+
+void MainWindow::cleanUpThread()
+{
+    worker_thread_->wait();
+    delete worker_thread_;
+    worker_thread_ = NULL;
+}
+
+void MainWindow::setProgressBar(int value)
+{
+    progress_bar_->setValue(value);
+}
+
+void MainWindow::rotateLogo()
+{
+    logo_rotation_timer = new QTimer;
+    connect(logo_rotation_timer, SIGNAL(timeout()),
+            render_area_, SLOT(updateLogo()));
+    logo_rotation_timer->start(40);
+}
+
+void MainWindow::resetLogo()
+{
+    logo_rotation_timer->stop();
+    delete logo_rotation_timer;
+    logo_rotation_timer = NULL;
+    render_area_->resetLogo();
 }
 
 RenderArea::RenderArea(QWidget *parent)
@@ -77,6 +147,19 @@ RenderArea::RenderArea(QWidget *parent)
 {
     QByteArray logo((const char *) test_svg, test_svg_len);
     svg_renderer_ = new QSvgRenderer(logo, this);
+}
+
+void RenderArea::updateLogo()
+{
+    rotation_state += G_PI / 20;
+    if (rotation_state >= 2.0 * G_PI) rotation_state = 0.0;
+    repaint();
+}
+
+void RenderArea::resetLogo()
+{
+    rotation_state = 0.0;
+    repaint();
 }
 
 void RenderArea::paintEvent(QPaintEvent *event)
@@ -89,7 +172,7 @@ void RenderArea::paintEvent(QPaintEvent *event)
     qreal canvas_aspect = ws.width() / ws.height();
 
     painter.translate( ws.width() / 2.0,  ws.height() / 2.0);
-    painter.rotate(rotation_state);
+    painter.rotate(rotation_state / G_PI * 180.0);
     painter.translate(-ws.width() / 2.0, -ws.height() / 2.0);
     QSizeF ns = ws;
     ns.setWidth(ws.width() * svg_aspect / canvas_aspect * scale_factor);
@@ -102,9 +185,98 @@ void RenderArea::paintEvent(QPaintEvent *event)
     svg_renderer_->render(&painter);
 }
 
+WorkerThread::WorkerThread(QList<QUrl> const& urls)
+    : QThread(NULL),
+      urls_(urls)
+{}
+
+void WorkerThread::run()
+{
+    std::vector<char *> roots;
+    for (QList<QUrl>::ConstIterator it = urls_.begin(); it != urls_.end(); ++it) {
+        roots.push_back(g_strdup(it->toLocalFile().toStdString().c_str()));
+    }
+    GSList *errors = NULL, *files = NULL;
+    Filetree tree = filetree_init(&roots[0], roots.size(), TRUE, FALSE, FALSE, &errors);
+
+    g_slist_foreach(errors, filetree_print_error, &verbose);
+    g_slist_foreach(errors, filetree_free_error, NULL);
+    g_slist_free(errors);
+
+    filetree_file_list(tree, &files);
+    filetree_remove_common_prefix(files);
+
+    int result = scan_files(files);
+    // if (result) {
+    //     g_idle_add((GSourceFunc) show_result_list, wd);
+    // } else {
+    //     wd->result_window = NULL;
+    //     destroy_work_data(wd);
+    // }
+
+    g_slist_foreach(files, filetree_free_list_entry, NULL);
+    g_slist_free(files);
+    filetree_destroy(tree);
+}
+
+GUIUpdateThread::GUIUpdateThread(QWidget *parent)
+    : QThread(parent),
+      old_progress_bar_value_(0),
+      rotation_active_(false),
+      stop_thread_(false)
+{}
+
+void GUIUpdateThread::run()
+{
+    for (;;) {
+        g_mutex_lock(progress_mutex);
+        g_cond_wait(progress_cond, progress_mutex);
+        if (stop_thread_) {
+            g_mutex_unlock(progress_mutex);
+            break;
+        }
+        if (total_frames > 0) {
+            if (!rotation_active_ && elapsed_frames) {
+                rotation_active_ = true;
+                emit rotateLogo();
+            }
+            int new_value = int(CLAMP(double(elapsed_frames) /
+                                      double(total_frames), 0.0, 1.0)
+                              * 130.0 + 0.5);
+            if (new_value != old_progress_bar_value_) {
+                emit setProgressBar(new_value);
+                old_progress_bar_value_ = new_value;
+            }
+            if (total_frames == elapsed_frames) {
+                emit setProgressBar(0);
+                old_progress_bar_value_ = 0;
+                rotation_active_ = false;
+                emit resetLogo();
+            }
+        }
+        g_mutex_unlock(progress_mutex);
+    }
+}
+
+void GUIUpdateThread::stopThread()
+{
+    stop_thread_ = true;
+    while (isRunning()) {
+        g_cond_broadcast(progress_cond);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    /* initialization */
+    g_thread_init(NULL);
+    input_init(argv[0], NULL);
+    scanner_init_common();
+    setlocale(LC_COLLATE, "");
+    setlocale(LC_CTYPE, "");
+
     MainWindow window;
     window.show();
     return app.exec();
--- scanner/scanner-drop-qt.h
+++ scanner/scanner-drop-qt.h
@@ -5,25 +5,68 @@ class RenderArea : public QWidget {
     Q_OBJECT
 public:
     RenderArea(QWidget *parent = NULL);
-    qreal rotation_state;
+public slots:
+    void updateLogo();
+    void resetLogo();
 protected:
     void paintEvent(QPaintEvent *event);
 private:
     QSvgRenderer *svg_renderer_;
+    qreal rotation_state;
+};
+
+class WorkerThread : public QThread {
+    Q_OBJECT
+public:
+    WorkerThread(QList<QUrl> const& files);
+protected:
+    void run();
+private:
+    QList<QUrl> urls_;
+};
+
+class GUIUpdateThread : public QThread {
+    Q_OBJECT
+public:
+    GUIUpdateThread(QWidget *parent);
+public slots:
+    void stopThread();
+protected:
+    void run();
+signals:
+    void rotateLogo();
+    void resetLogo();
+    void setProgressBar(int value);
+private:
+    int old_progress_bar_value_;
+    bool rotation_active_;
+    bool stop_thread_;
 };
 
 class MainWindow : public QWidget {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = NULL);
+    ~MainWindow();
     QSize sizeHint() const;
 protected:
     void mousePressEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
     void mouseReleaseEvent(QMouseEvent *event);
-    void resizeEvent(QResizeEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+private slots:
+    void cleanUpThread();
+    void setProgressBar(int);
+    void rotateLogo();
+    void resetLogo();
+signals:
+    void stopGUIThread();
 private:
     QPoint dragPosition;
     RenderArea *render_area_;
     QProgressBar *progress_bar_;
+    WorkerThread *worker_thread_;
+    GUIUpdateThread gui_update_thread_;
+    QTimer *logo_rotation_timer;
 };
